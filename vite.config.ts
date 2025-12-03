66f6a935c2629c5086f52b38e65ea549f91746e8
komu <sahukomendra721@gmail.com>
2025-12-03 18:40:35 +0530

    added path in vite file

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/my_road_safety_predictor/",   // ⭐ ADD THIS LINE ONLY
+  
   server: {
     host: "::",
     port: 8080,
